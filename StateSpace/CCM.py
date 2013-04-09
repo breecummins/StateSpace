453632d661fc308851d26d9110fc2408f5e3ccd0
Bree Cummins <breecummins@gmail.com>
2013-04-09 16:34:39 -0600

    rearranged functions and comments

--- StateSpace/CCM.py
+++ StateSpace/CCM.py
@@ -4,7 +4,6 @@ import numpy as np
 def findClosest(poi,pts,N):
     '''
     Find the closest N points in pts (numpy array) to poi.
-    This method is faster than either of the other two below.
 
     '''
     dists = np.sqrt(((pts - poi)**2).sum(1))
@@ -15,55 +14,35 @@ def findClosest(poi,pts,N):
         dists[i] = np.Inf
     return zip(*out)
 
-# def findClosest2(poi,pts,N):
-#     '''
-#     Find the closest N points in pts (numpy array) to poi.
-
-#     '''
-#     dists = np.sqrt(((pts - poi)**2).sum(1))
-#     inds = dists.argsort()[:N]
-#     return [(dists[i],i) for i in inds]
-
-# def findClosest3(poi,pts,N):
-#     '''
-#     Find the closest N points in pts (numpy array) to poi.
-
-#     '''
-#     dists = np.sqrt(((pts - poi)**2).sum(1))
-#     inds = bn.argpartsort(dists,N)[:N]
-#     d = [(dists[i],i) for i in inds]
-#     d.sort()
-#     return d
-
 def makeExpWeights(dists):
     u = np.exp( -dists/dists[0])
     return u / u.sum()
 
-def estSeries(M,ts,npts,wgtfunc=makeExpWeights):
-    est=np.zeros(ts.shape)
-    for k in range(M.shape[0]):
-        poi = M[k,:]
-        dists,inds = findClosest(poi,M,npts)
-        w = wgtfunc(dists)
-        est[k] = w*ts[list(inds)]
-    return est
-
-def crossMap(ts1,ts2,numlags,lagsize,wgtfunc=makeExpWeights):
+def crossMap(ts1,ts2,numlags,lagsize,wgtfunc):
     '''
     Estimate timeseries 1 (ts1) from timeseries 2 and vice versa using a 
-    cross-mapping technique.
-    numlags is the dimension of the embedding space for the reconstruction.
-    Use time lags of size lagsize * dt to construct shadow manifolds. lagsize
-    is an integer representing the index of the time lag.
+    cross-mapping technique between shadow manifolds.
+    Construct a shadow manifold M from one time series.
+    Find the nearest points to each point in M.
+    Construct weights based on their distances to the point of
+    interest. 
+    Use the time indices of these points and the associated weights to 
+    make a weighted-sum estimate of the contemporaneous points in the 
+    other time series.
 
     '''
-    # construct the shadow manifolds
     M1 = SSR.makeShadowManifold(timeseries1,numlags,lagsize)
     M2 = SSR.makeShadowManifold(timeseries2,numlags,lagsize)
-    # reconstruct 1 from 2 (testing to see if 1 -> 2)
-    est1 = estSeries(M2,M1[:,-1],numlags+1,wgtfunc)
-    # reconstruct 2 from 1 (testing to see if 2 -> 1)
-    est2 = estSeries(M1,M2[:,-1],numlags+1,wgtfunc)
+    def estSeries(M,ts):
+        est=np.zeros(ts.shape)
+        for k in range(M.shape[0]):
+            poi = M[k,:]
+            dists,inds = findClosest(poi,M,numlags+1)
+            w = wgtfunc(dists)
+            est[k] = w*ts[list(inds)]
+        return est
+    est1 = estSeries(M2,M1[:,-1])
+    est2 = estSeries(M1,M2[:,-1])
     return est1, est2
 
 def corrCoeff():
@@ -72,6 +51,15 @@ def corrCoeff():
 def testCausality(ts1,ts2,numlags,lagsize,listoflens,numiters,wgtfunc=makeExpWeights):
     '''
     Check for convergence (Sugihara) to infer causality between ts1 and ts2.
+    numlags is the dimension of the embedding space for the reconstruction.
+    Use time lags of size lagsize * dt to construct shadow manifolds. lagsize
+    is an integer representing the index of the time lag.
+    listoflens contains the lengths to use to show convergence 
+    Example: range(100,10000,100)
+    Each length will be run numiters times from different random starting 
+    locations in the time series.
+    The estimated time series will be constructed using the weighting function 
+    handle given by wgtfunc.
 
     '''
     listoflens.sort()
