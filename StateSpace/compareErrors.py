57743be78af980593a5afb910999868f2a1db0c8
Bree Cummins <breecummins@gmail.com>
2013-06-04 09:57:49 -0600

    changed parameters in Sugihara comparison for a really long and refined times series on the double pendulum

--- StateSpace/compareErrors.py
+++ StateSpace/compareErrors.py
@@ -11,31 +11,31 @@ import random
 # from differenceEqns import solve2Species
 # timeseries = solve2Species([0.4,0.2],80.0)
 from DoublePendulum import solvePendulum
-dt = 0.1
-timeseries = solvePendulum([1.0,2.0,3.0,2.0],300.0,dt=dt)
+dt = 0.025
+timeseries = solvePendulum([1.0,2.0,3.0,2.0],600.0,dt=dt)
 # quantities needed for the different methods
 eqns = 'Double pendulum'
 names = ['x','y','z','w']
 numlags=4
-lagsize=8 
+lagsize=32 
 compind1 = 2
 compind2 = 3
-startind = 0
+startind = 500
 endind=len(timeseries)
 corr = (numlags-1)*lagsize
-M1=SSR.makeShadowManifold(timeseries[startind:endind,compind1],numlags,lagsize)
-M2=SSR.makeShadowManifold(timeseries[startind:endind,compind2],numlags,lagsize)
-#function defs
-def calcErrs(M1est,M2est,method,M1ref=M1[corr:,:],M2ref=M2[corr:,:]):
-    err1 = method(M1ref,M1est)
-    err2 = method(M2ref,M2est)
-    return err1,err2
+# M1=SSR.makeShadowManifold(timeseries[startind:endind,compind1],numlags,lagsize)
+# M2=SSR.makeShadowManifold(timeseries[startind:endind,compind2],numlags,lagsize)
+# #function defs
+# def calcErrs(M1est,M2est,method,M1ref=M1[corr:,:],M2ref=M2[corr:,:]):
+#     err1 = method(M1ref,M1est)
+#     err2 = method(M2ref,M2est)
+#     return err1,err2
 
-def printMe(method,err1,err2,name1='M'+names[compind1],name2='M'+names[compind2]):
-    print("    "+ method+" between M"+name1+" and M"+name1+"' is " + str(err1))
-    print("    "+ method+" between M"+name2+" and M"+name2+"' is " + str(err2))
+# def printMe(method,err1,err2,name1='M'+names[compind1],name2='M'+names[compind2]):
+#     print("    "+ method+" between M"+name1+" and M"+name1+"' is " + str(err1))
+#     print("    "+ method+" between M"+name2+" and M"+name2+"' is " + str(err2))
 
-print(eqns+' with lagsize of '+str(lagsize)+'*dt with dt = '+str(dt)+' and reconstruction dimension '+str(numlags))
+# print(eqns+' with lagsize of '+str(lagsize)+'*dt with dt = '+str(dt)+' and reconstruction dimension '+str(numlags))
 
 # # Measure whole manifold
 
@@ -78,23 +78,19 @@ print(eqns+' with lagsize of '+str(lagsize)+'*dt with dt = '+str(dt)+' and recon
 
 # Construct a series of manifolds
 
-listoflens = range(400,2100,400)
+listoflens = range(1000,22600,1000)
 numiters = 25
 
 print('The number of iterations per time series length is {0!s}'.format(numiters))
 
 def printResults(note,short,avg1,std1,avg2,std2,name1='M'+names[compind1],name2='M'+names[compind2]):
     print(note + " between " + name1 + " and " + name1 +"': " + ' '.join(["{0:0.6f}".format(i) for i in avg1]))
-    print("Standard deviations for " + short + ' ' + name1 + " and " + name1 +"': " + ' '.join(["{0:0.6f}".format(i) for i in std1]))
+    # print("Standard deviations for " + short + ' ' + name1 + " and " + name1 +"': " + ' '.join(["{0:0.6f}".format(i) for i in std1]))
     print(note + " between " + name2 + " and " + name2 +"': " + ' '.join(["{0:0.6f}".format(i) for i in avg2]))
-    print("Standard deviations for " + short + ' ' + name2 + " and " + name2 +"': " + ' '.join(["{0:0.6f}".format(i) for i in std2]))
+    # print("Standard deviations for " + short + ' ' + name2 + " and " + name2 +"': " + ' '.join(["{0:0.6f}".format(i) for i in std2]))
 
 # Sugihara method with corr coeff
 lol,avgs1,avgs2,stds1,stds2 = CCM.testCausality(timeseries[startind:endind,compind1],timeseries[startind:endind,compind2],numlags,lagsize,listoflens,numiters,Weights.makeExpWeights,[Similarity.corrCoeffPearson,Similarity.RootMeanSquaredErrorTS])
-print(avgs1)
-print(avgs2)
-print(stds1)
-print(stds2)
 print('############################################################################')
 print("Sugihara method with correlation coefficient:")
 print("Lengths: " + str(lol))
@@ -103,61 +99,61 @@ print('#########################################################################
 print("Sugihara method with root mean square error:")
 print("Lengths: " + str(lol))
 printResults("Mean time series RMSE", "RMSE", [avgs1[_k][1] for _k in range(len(avgs1))],[stds1[_k][1] for _k in range(len(avgs1))],[avgs2[_k][1] for _k in range(len(avgs1))],[stds2[_k][1] for _k in range(len(avgs1))],names[compind1],names[compind2])
-# # Sugihara with intermediate reconstruction
-# avgs1 = []
-# stds1 = []
-# avgs2 = []
-# stds2 = []
-# for l in listoflens:
-#     startinds = [ s + startind for s in random.sample(range(endind-startind-l),numiters)]
-#     a1=[]
-#     a2=[]
-#     for s in startinds:
-#         M1orig=SSR.makeShadowManifold(timeseries[s:s+l,compind1],numlags,lagsize)
-#         M2orig=SSR.makeShadowManifold(timeseries[s:s+l,compind2],numlags,lagsize)
-#         est1,est2=CCM.crossMapManifold(M1orig,M2orig,numlags,lagsize,Weights.makeExpWeights)
-#         M1Sug=SSR.makeShadowManifold(est1,numlags,lagsize)
-#         M2Sug=SSR.makeShadowManifold(est2,numlags,lagsize)
-#         M1SugRMSE, M2SugRMSE = calcErrs(M1Sug,M2Sug,Similarity.RootMeanSquaredErrorManifold,M1ref=M1orig[corr:,:],M2ref=M2orig[corr:,:])
-#         M1SugHD, M2SugHD = calcErrs(M1Sug,M2Sug,Similarity.HausdorffDistance,M1ref=M1orig[corr:,:],M2ref=M2orig[corr:,:])
-#         M1SugME, M2SugME = calcErrs(M1Sug,M2Sug,Similarity.MeanErrorManifold,M1ref=M1orig[corr:,:],M2ref=M2orig[corr:,:])
-#         a1.extend([M1SugRMSE,M1SugME,M1SugHD])
-#         a2.extend([M2SugRMSE,M2SugME,M2SugHD])
-#     avgs1.append([np.mean(a1[_k::3]) for _k in range(3)])
-#     stds1.append([np.std(a1[_k::3]) for _k in range(3)])
-#     avgs2.append([np.mean(a2[_k::3]) for _k in range(3)])
-#     stds2.append([np.std(a2[_k::3]) for _k in range(3)])
-# print('############################################################################')
-# print("Sugihara method with intermediate reconstruction:")
-# print("Lengths: " + str(listoflens))
-# printResults("Mean RMSE","RMSE",[avgs1[_k][0] for _k in range(len(avgs1))],[stds1[_k][0] for _k in range(len(avgs1))],[avgs2[_k][0] for _k in range(len(avgs1))],[stds2[_k][0] for _k in range(len(avgs1))])
-# printResults("Mean error per point","ME",[avgs1[_k][1] for _k in range(len(avgs1))],[stds1[_k][1] for _k in range(len(avgs1))],[avgs2[_k][1] for _k in range(len(avgs1))],[stds2[_k][1] for _k in range(len(avgs1))])
-# printResults("Mean Hausdorff distance","ME",[avgs1[_k][2] for _k in range(len(avgs1))],[stds1[_k][2] for _k in range(len(avgs1))],[avgs2[_k][2] for _k in range(len(avgs1))],[stds2[_k][2] for _k in range(len(avgs1))])
+# Sugihara with intermediate reconstruction
+avgs1 = []
+stds1 = []
+avgs2 = []
+stds2 = []
+for l in listoflens:
+    startinds = [ s + startind for s in random.sample(range(endind-startind-l),numiters)]
+    a1=[]
+    a2=[]
+    for s in startinds:
+        M1orig=SSR.makeShadowManifold(timeseries[s:s+l,compind1],numlags,lagsize)
+        M2orig=SSR.makeShadowManifold(timeseries[s:s+l,compind2],numlags,lagsize)
+        est1,est2=CCM.crossMapManifold(M1orig,M2orig,numlags,lagsize,Weights.makeExpWeights)
+        M1Sug=SSR.makeShadowManifold(est1,numlags,lagsize)
+        M2Sug=SSR.makeShadowManifold(est2,numlags,lagsize)
+        M1SugRMSE, M2SugRMSE = calcErrs(M1Sug,M2Sug,Similarity.RootMeanSquaredErrorManifold,M1ref=M1orig[corr:,:],M2ref=M2orig[corr:,:])
+        M1SugHD, M2SugHD = calcErrs(M1Sug,M2Sug,Similarity.HausdorffDistance,M1ref=M1orig[corr:,:],M2ref=M2orig[corr:,:])
+        M1SugME, M2SugME = calcErrs(M1Sug,M2Sug,Similarity.MeanErrorManifold,M1ref=M1orig[corr:,:],M2ref=M2orig[corr:,:])
+        a1.extend([M1SugRMSE,M1SugME,M1SugHD])
+        a2.extend([M2SugRMSE,M2SugME,M2SugHD])
+    avgs1.append([np.mean(a1[_k::3]) for _k in range(3)])
+    stds1.append([np.std(a1[_k::3]) for _k in range(3)])
+    avgs2.append([np.mean(a2[_k::3]) for _k in range(3)])
+    stds2.append([np.std(a2[_k::3]) for _k in range(3)])
+print('############################################################################')
+print("Sugihara method with intermediate reconstruction:")
+print("Lengths: " + str(listoflens))
+printResults("Mean RMSE","RMSE",[avgs1[_k][0] for _k in range(len(avgs1))],[stds1[_k][0] for _k in range(len(avgs1))],[avgs2[_k][0] for _k in range(len(avgs1))],[stds2[_k][0] for _k in range(len(avgs1))])
+printResults("Mean error per point","ME",[avgs1[_k][1] for _k in range(len(avgs1))],[stds1[_k][1] for _k in range(len(avgs1))],[avgs2[_k][1] for _k in range(len(avgs1))],[stds2[_k][1] for _k in range(len(avgs1))])
+printResults("Mean Hausdorff distance","ME",[avgs1[_k][2] for _k in range(len(avgs1))],[stds1[_k][2] for _k in range(len(avgs1))],[avgs2[_k][2] for _k in range(len(avgs1))],[stds2[_k][2] for _k in range(len(avgs1))])
 
-# # weighted sums in the embedding space
-# lol,avgs1,avgs2,stds1,stds2 = CCMAlternatives.testCausalityReconstruction(timeseries[startind:endind,compind1],timeseries[startind:endind,compind2],numlags,lagsize,listoflens,numiters,wgtfunc=Weights.makeExpWeights,simMeasure=[Similarity.RootMeanSquaredErrorManifold,Similarity.MeanErrorManifold,Similarity.HausdorffDistance])
-# print('############################################################################')
-# print("Direct estimation of the manifold using exponential weights:")
-# print("Lengths: " + str(listoflens))
-# printResults("Mean RMSE","RMSE",[avgs1[_k][0] for _k in range(len(avgs1))],[stds1[_k][0] for _k in range(len(avgs1))],[avgs2[_k][0] for _k in range(len(avgs1))],[stds2[_k][0] for _k in range(len(avgs1))])
-# printResults("Mean error per point","ME",[avgs1[_k][1] for _k in range(len(avgs1))],[stds1[_k][1] for _k in range(len(avgs1))],[avgs2[_k][1] for _k in range(len(avgs1))],[stds2[_k][1] for _k in range(len(avgs1))])
-# printResults("Mean Hausdorff distance","ME",[avgs1[_k][2] for _k in range(len(avgs1))],[stds1[_k][2] for _k in range(len(avgs1))],[avgs2[_k][2] for _k in range(len(avgs1))],[stds2[_k][2] for _k in range(len(avgs1))])
+# weighted sums in the embedding space
+lol,avgs1,avgs2,stds1,stds2 = CCMAlternatives.testCausalityReconstruction(timeseries[startind:endind,compind1],timeseries[startind:endind,compind2],numlags,lagsize,listoflens,numiters,wgtfunc=Weights.makeExpWeights,simMeasure=[Similarity.RootMeanSquaredErrorManifold,Similarity.MeanErrorManifold,Similarity.HausdorffDistance])
+print('############################################################################')
+print("Direct estimation of the manifold using exponential weights:")
+print("Lengths: " + str(listoflens))
+printResults("Mean RMSE","RMSE",[avgs1[_k][0] for _k in range(len(avgs1))],[stds1[_k][0] for _k in range(len(avgs1))],[avgs2[_k][0] for _k in range(len(avgs1))],[stds2[_k][0] for _k in range(len(avgs1))])
+printResults("Mean error per point","ME",[avgs1[_k][1] for _k in range(len(avgs1))],[stds1[_k][1] for _k in range(len(avgs1))],[avgs2[_k][1] for _k in range(len(avgs1))],[stds2[_k][1] for _k in range(len(avgs1))])
+printResults("Mean Hausdorff distance","ME",[avgs1[_k][2] for _k in range(len(avgs1))],[stds1[_k][2] for _k in range(len(avgs1))],[avgs2[_k][2] for _k in range(len(avgs1))],[stds2[_k][2] for _k in range(len(avgs1))])
 
-# lol,avgs1,avgs2,stds1,stds2 = CCMAlternatives.testCausalityReconstruction(timeseries[startind:endind,compind1],timeseries[startind:endind,compind2],numlags,lagsize,listoflens,numiters,wgtfunc=Weights.makeUniformWeights,simMeasure=[Similarity.RootMeanSquaredErrorManifold,Similarity.MeanErrorManifold,Similarity.HausdorffDistance])
-# print('############################################################################')
-# print("Direct estimation of the manifold using uniform weights:")
-# print("Lengths: " + str(listoflens))
-# printResults("Mean RMSE","RMSE",[avgs1[_k][0] for _k in range(len(avgs1))],[stds1[_k][0] for _k in range(len(avgs1))],[avgs2[_k][0] for _k in range(len(avgs1))],[stds2[_k][0] for _k in range(len(avgs1))])
-# printResults("Mean error per point","ME",[avgs1[_k][1] for _k in range(len(avgs1))],[stds1[_k][1] for _k in range(len(avgs1))],[avgs2[_k][1] for _k in range(len(avgs1))],[stds2[_k][1] for _k in range(len(avgs1))])
-# printResults("Mean Hausdorff distance","ME",[avgs1[_k][2] for _k in range(len(avgs1))],[stds1[_k][2] for _k in range(len(avgs1))],[avgs2[_k][2] for _k in range(len(avgs1))],[stds2[_k][2] for _k in range(len(avgs1))])
+lol,avgs1,avgs2,stds1,stds2 = CCMAlternatives.testCausalityReconstruction(timeseries[startind:endind,compind1],timeseries[startind:endind,compind2],numlags,lagsize,listoflens,numiters,wgtfunc=Weights.makeUniformWeights,simMeasure=[Similarity.RootMeanSquaredErrorManifold,Similarity.MeanErrorManifold,Similarity.HausdorffDistance])
+print('############################################################################')
+print("Direct estimation of the manifold using uniform weights:")
+print("Lengths: " + str(listoflens))
+printResults("Mean RMSE","RMSE",[avgs1[_k][0] for _k in range(len(avgs1))],[stds1[_k][0] for _k in range(len(avgs1))],[avgs2[_k][0] for _k in range(len(avgs1))],[stds2[_k][0] for _k in range(len(avgs1))])
+printResults("Mean error per point","ME",[avgs1[_k][1] for _k in range(len(avgs1))],[stds1[_k][1] for _k in range(len(avgs1))],[avgs2[_k][1] for _k in range(len(avgs1))],[stds2[_k][1] for _k in range(len(avgs1))])
+printResults("Mean Hausdorff distance","ME",[avgs1[_k][2] for _k in range(len(avgs1))],[stds1[_k][2] for _k in range(len(avgs1))],[avgs2[_k][2] for _k in range(len(avgs1))],[stds2[_k][2] for _k in range(len(avgs1))])
 
-# lol,avgs1,avgs2,stds1,stds2 = CCMAlternatives.testCausalityReconstruction(timeseries[startind:endind,compind1],timeseries[startind:endind,compind2],numlags,lagsize,listoflens,numiters,wgtfunc=Weights.makeLambdaWeights,simMeasure=[Similarity.RootMeanSquaredErrorManifold,Similarity.MeanErrorManifold,Similarity.HausdorffDistance])
-# print('############################################################################')
-# print("Direct estimation of the manifold using weights made from powers of 1/2:")
-# print("Lengths: " + str(listoflens))
-# printResults("Mean RMSE","RMSE",[avgs1[_k][0] for _k in range(len(avgs1))],[stds1[_k][0] for _k in range(len(avgs1))],[avgs2[_k][0] for _k in range(len(avgs1))],[stds2[_k][0] for _k in range(len(avgs1))])
-# printResults("Mean error per point","ME",[avgs1[_k][1] for _k in range(len(avgs1))],[stds1[_k][1] for _k in range(len(avgs1))],[avgs2[_k][1] for _k in range(len(avgs1))],[stds2[_k][1] for _k in range(len(avgs1))])
-# printResults("Mean Hausdorff distance","ME",[avgs1[_k][2] for _k in range(len(avgs1))],[stds1[_k][2] for _k in range(len(avgs1))],[avgs2[_k][2] for _k in range(len(avgs1))],[stds2[_k][2] for _k in range(len(avgs1))])
+lol,avgs1,avgs2,stds1,stds2 = CCMAlternatives.testCausalityReconstruction(timeseries[startind:endind,compind1],timeseries[startind:endind,compind2],numlags,lagsize,listoflens,numiters,wgtfunc=Weights.makeLambdaWeights,simMeasure=[Similarity.RootMeanSquaredErrorManifold,Similarity.MeanErrorManifold,Similarity.HausdorffDistance])
+print('############################################################################')
+print("Direct estimation of the manifold using weights made from powers of 1/2:")
+print("Lengths: " + str(listoflens))
+printResults("Mean RMSE","RMSE",[avgs1[_k][0] for _k in range(len(avgs1))],[stds1[_k][0] for _k in range(len(avgs1))],[avgs2[_k][0] for _k in range(len(avgs1))],[stds2[_k][0] for _k in range(len(avgs1))])
+printResults("Mean error per point","ME",[avgs1[_k][1] for _k in range(len(avgs1))],[stds1[_k][1] for _k in range(len(avgs1))],[avgs2[_k][1] for _k in range(len(avgs1))],[stds2[_k][1] for _k in range(len(avgs1))])
+printResults("Mean Hausdorff distance","ME",[avgs1[_k][2] for _k in range(len(avgs1))],[stds1[_k][2] for _k in range(len(avgs1))],[avgs2[_k][2] for _k in range(len(avgs1))],[stds2[_k][2] for _k in range(len(avgs1))])
 
 
 
