fixed bugs in Cao neighbor method and compareErrors. Added lagsize calculation from autocorrelation to StateSpaceReconstruction. Finished writing PecoraMethod and wrote PecoraViz to test it. Testing now.

StateSpace/CaoNeighborRatio.py
@@ -6,14 +6,15 @@ import StateSpaceReconstruction as SSR
 def lagFromFirstZeroAutocorrelation(ts,M=None):
     '''
     Calculate autocorrelation of a 1D timeseries and 
-    return the first zero crossing.
+    return the first zero crossing. Check all lags from
+    1 to M+1. Default is M = len(ts)/10. 
 
     '''
     mu = np.mean(ts)
     s2 = np.var(ts,ddof=1) #unbiased estimator of variance
     N = len(ts)
     if M == None:
-        M = int(np.floor(N/10.))
+        M = int(N/10.)
     autocc = []
     for k in range(1,M+1):
         autocc.append( ((ts[:N-k] - mu)*(ts[k:] - mu)).sum() / ( s2 *(N-k) ) )
@@ -24,21 +25,18 @@ def findFirstZero(arr):
     arr must be a list or 1D numpy array
 
     '''
-    arr = np.array(arr)
-    up = np.nonzero(arr >= 0)[0]
-    down = np.nonzero(arr < 0)[0]
-    for i in up:
-        if i+1 in down:
-            if np.abs(arr[i]) >= np.abs(arr[i+1]):
-                return i+1
-            else:
-                return i
-        if i-1 in down:
-            if np.abs(arr[i]) >= np.abs(arr[i-1]):
-                return i-1
-            else:
-                return i
-    return None
+    signs = np.sign(arr[1:]) + np.sign(arr[:-1])
+    ind = np.argmax(np.abs(signs) < 2)
+    if ind == 0 and np.abs(signs[0]) == 2: 
+        # False positive: if there is no zero crossing, argmax will return 
+        # the first index, so I have to manually return None.
+        return None 
+    elif np.abs(arr[ind]) < np.abs(arr[ind+1]):
+        # return whichever of the two indices flanking zero has the smallest value
+        return ind
+    else:
+        return ind+1
+
 
 def Ln(poi,pts,n=None):
     '''

StateSpace/PecoraMethod.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.misc import comb
+import random
 
 def getBinomialMax(n,p):
     '''
@@ -11,15 +12,16 @@ def getBinomialMax(n,p):
     maxloc = np.floor( (n+1)*p )
     return comb(n,maxloc)*(p**maxloc)*((1-p)**(n-maxloc)) 
 
-def getContinuityConfidence(neps,ndelta,N):
+def getContinuityConfidence(neps,ndelta,numpts):
     '''
     Compare the probability of observing that all ndelta points
     map to the epsilon set with the null hypthesis that the
     points are randomly distributed. Random distribution 
-    corresponds to neps/N probability per point, where neps
+    corresponds to neps/numpts probability per point, where neps
     is the number of points observed within eps of the image
-    point. Successes (falling within eps of the image) should
-    follow a binomial distribution for a random distribution.
+    point and numpts is the total number of points in the 
+    reconstruction. Successes (falling within eps of the image) should
+    follow a binomial distribution for an underlyning random distribution.
     If ndelta is large enough to be unlikely, then we can reject
     the null hypothesis of random distribution. Since the points
     are consistent with the definition of continuity, we say we 
@@ -27,17 +29,103 @@ def getContinuityConfidence(neps,ndelta,N):
     continuous function.
 
     '''
-    p = float(neps) / N
+    p = float(neps) / numpts
     pmax = getBinomialMax(ndelta,p)
     return 1 - (p**ndelta)/pmax
 
-def chooseEpsilons(M):
+def countPtsWithinEps(M,ind,eps):
+    '''
+    M is an mxn numpy array with one n-dimensional point on each
+    row. Count the number of points within eps of M[ind,:]. Exclude 
+    the M[ind,:] point itself by subtracting 1 from the count.
+
+    '''
+    dists = np.sqrt(((M - M[ind,:])**2).sum(1))
+    return (dists < eps).sum() - 1 
+
+def findPtsWithinDelta(M,ind,delta):
+    '''
+    M is an mxn numpy array with one n-dimensional point on each
+    row. Find the indices of the points within delta of M[ind,:]. 
+    Exclude ind from the returned indices.
+
+    '''
+    dists = np.sqrt(((M - M[ind,:])**2).sum(1))
+    inds = list(np.nonzero(dists < delta)[0])
+    return inds.remove(ind)
+
+def countDeltaPtsMappedToEps(M1,M2,delta,eps,ind):
+    '''
+    Find all points within delta of M1[ind,:], then check to see
+    if their images fall within eps of M2[ind,:]. If they all do,
+    we have a success and return the number of points within delta.
+    If we fail, then return False (delta is too big). If there are 
+    no points within delta, then return None (delta is too small).
+
+    '''
+    deltainds = findPtsWithinDelta(M1,ind,delta)
+    if deltainds == None:
+        return None
+    for k in deltainds:
+        if np.sqrt(((M2[k,:] - M2[ind,:])**2).sum(1)) >= eps:
+            return False
+    return len(deltainds)
+
+def continuityTest(M1,M2,ptinds,eps,startdelta):
+    '''
+    Do Pecora continuity test on the reconstructions M1 and M2 using 
+    the points with indices ptinds and continuity parameter eps. 
+    Guess delta values beginning with startdelta, which ideally has the
+    relation startdelta/|M1| is approximately eps/|M2|.
+
+    '''
+    contstat = np.zeros(len(ptinds))
+    for k,ind in enumerate(ptinds):
+        neps = countPtsWithinEps(M2,ind,eps)
+        if neps > 0: # if eps big enough, continue; else leave 0 in place
+            delta = 2*startdelta
+            out = False
+            while out is False:
+                delta = delta*0.5
+                out = countDeltaPtsMappedToEps(M1,M2,delta,eps,ind)                
+            if out: #out can be None, in which case we want to leave zero in contstat
+                contstat[k] = getContinuityConfidence(neps,out,M1.shape[0])
+    return np.mean(contstat)
+
+def chooseEpsilons(M,mastereps):
     '''
     Estimate the standard deviation of the manifold M (sensu Pecora)
-    and take fractions of it for different epsilons.
+    and take fractions of it for different epsilons. The proportions
+    to take are given in mastereps, a numpy array.
 
     '''
     meanM = np.mean(M,axis=0)
     dists = np.sqrt(((M - meanM)**2).sum(axis=1))
     stdM = np.std(dists)
-    return std*np.array([0.01,0.02,0.05,0.1,0.2])
+    return stdM*mastereps
+
+def convergenceWithContinuityTest(M1,M2,Nlist,mastereps=np.array([0.005,0.01,0.02,0.05,0.1,0.2])):
+    '''
+    Do Pecora method (continuity and inverse continuity) on the 
+    reconstructions M1 and M2, which are mxn numpy arrays of m 
+    points in n dimensions. 
+    Method is performed on different numbers of random points given
+    in Nlist. We are checking for convergence patterns in N and in
+    epsilon values for the continuity test. The different epsilons 
+    to try are calculated as proportions of the standard deviation 
+    of the distances of points in M1 and M2 from their respective
+    mean values. 
+
+    '''
+    epslist1 = chooseEpsilons(M2,mastereps) # M2 is range in forward continuity 
+    epslist2 = chooseEpsilons(M1,mastereps) # M1 is range in inverse continuity
+    contconf = np.zeros((len(Nlist),len(epslist1)))
+    invcontconf = np.zeros((len(Nlist),len(epslist2)))
+    for j,N in enumerate(Nlist):
+        ptinds = random.sample(range(M1.shape[0]),N)
+        for k in range(len(epslist1)):
+            contconf[j,k] = continuityTest(M1,M2,ptinds,epslist1[k],epslist2[k])
+            invcontconf[j,k] = continuityTest(M2,M1,ptinds,epslist2[k],epslist1[k])
+    return contconf, invcontconf
+
+