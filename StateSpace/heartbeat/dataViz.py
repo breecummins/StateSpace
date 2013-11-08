43715e25c03e4ae4cd990770b16a2e57749caf93
Bree Cummins <breecummins@gmail.com>
2013-11-08 12:43:21 -0700

    fixed missing import bug and factored out viz code

---
+++ StateSpace/heartbeat/dataViz.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import StateSpace.StateSpaceReconstructionPlots as SSRPlots
+import StateSpace.StateSpaceReconstruction as SSR
+import processPhysData as PD
+
+def plotRates(ts,start=0,stop=None):
+    if not stop:
+        stop = len(hr)
+    timeindex = range(start,stop)
+    plt.plot(timeindex,ts[start:stop,0],'r')
+    plt.hold('on')
+    plt.plot(timeindex,ts[start:stop,1],'b')
+    plt.legend(['Heart rate','Breathing rate'])
+    plt.xlim([start,stop])
+    plt.show()
+
+def autocorrViz(ts):
+    autocorr0 = SSR.getAutocorrelation(ts[:,0],int(0.1*ts.shape[0]))
+    autocorr1 = SSR.getAutocorrelation(ts[:,1],int(0.1*ts.shape[0]))
+    SSRPlots.plotAutocorrelation(autocorr0,"heart rate autocorr")
+    SSRPlots.plotAutocorrelation(autocorr1,"breathing rate autocorr")
+
+if __name__ == "__main__":
+    ts = PD.extractRates()
+    SSRPlots.plotShadowManifold(ts[:,0], 3, 20, show=0,hold=0,style='b-',titlestr='Heart rate',scatter=False, color=None,smooth=1)
+    SSRPlots.plotShadowManifold(ts[:,1], 3, 20, show=1,hold=0,style='b-',titlestr='Breathing rate',scatter=False, color=None,smooth=1)
+    autocorrViz(ts)
+    plotRates(ts,90,720)
+
--- StateSpace/heartbeat/processPhysData.py
+++ StateSpace/heartbeat/processPhysData.py
@@ -1,10 +1,8 @@
 import numpy as np
 import random
-# import matplotlib.pyplot as plt
-import StateSpace.StateSpaceReconstruction as SSR
-# import StateSpace.StateSpaceReconstructionPlots as SSRPlots
 import StateSpace.PecoraMethodModified as PM
 import StateSpace.CaoNeighborRatio as CNR
+import fileops
 
 def extractRates(fname='physdata_13081200_NewSummary.csv'):
     f = open(fname,'r')
@@ -18,17 +16,6 @@ def extractRates(fname='physdata_13081200_NewSummary.csv'):
         heartrate.append(float(words[3]))
         breathingrate.append(float(words[5]))
     return np.vstack([heartrate,breathingrate]).transpose()
-
-def plotRates(ts,start=0,stop=None):
-    if not stop:
-        stop = len(hr)
-    timeindex = range(start,stop)
-    plt.plot(timeindex,ts[start:stop,0],'r')
-    plt.hold('on')
-    plt.plot(timeindex,ts[start:stop,1],'b')
-    plt.legend(['Heart rate','Breathing rate'])
-    plt.xlim([start,stop])
-    plt.show()
 
 def getLagsDims(ts,lags=None):
     N = ts.shape[0]
@@ -54,12 +41,6 @@ def getLagsDimsWrapper():
     print("Lags = 100")
     print("--------------------------------------")
     lags,dims = getLagsDims(ts, lags=[100,100])
-
-def autocorrViz(ts):
-    autocorr0 = SSR.getAutocorrelation(ts[:,0],int(0.1*ts.shape[0]))
-    autocorr1 = SSR.getAutocorrelation(ts[:,1],int(0.1*ts.shape[0]))
-    SSRPlots.plotAutocorrelation(autocorr0,"heart rate autocorr")
-    SSRPlots.plotAutocorrelation(autocorr1,"breathing rate autocorr")
 
 def continuityTestingFixedEps(eqns,names,ts,compinds,tsprops,epsprops,lags,numlags,fname=''):
     forwardconf, inverseconf, epsM1, epsM2, forwardprobs, inverseprobs = PM.convergenceWithContinuityTestFixedLagsFixedEps(ts[:,compinds[0]],ts[:,compinds[1]],numlags,lags[0],lags[1],tsprops=tsprops,epsprops=epsprops)
@@ -94,12 +75,5 @@ def runHeartRate(lags,dims,remote=1):
 
 if __name__ == "__main__":
     runHeartRate([10,20,50],[3,4])
-    # ts = extractRates()
-    # SSRPlots.plotShadowManifold(ts[:,0], 3, 20, show=0,hold=0,style='b-',titlestr='Heart rate',scatter=False, color=None,smooth=1)
-    # SSRPlots.plotShadowManifold(ts[:,1], 3, 20, show=1,hold=0,style='b-',titlestr='Breathing rate',scatter=False, color=None,smooth=1)
-    # autocorrViz(ts)
-    # plotRates(ts,90,720)
-    # print("lags = {0}".format(lags))
-    # print("dims = {0}".format(dims))
 
 
