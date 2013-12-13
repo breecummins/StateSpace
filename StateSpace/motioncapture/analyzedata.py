9d6911ff0f194fc564f0beafad5495e2c8b3b12e
Bree Cummins <breecummins@gmail.com>
2013-12-13 16:22:23 -0700

    fixed indexing errors

--- StateSpace/motioncapture/analyzedata.py
+++ StateSpace/motioncapture/analyzedata.py
@@ -5,16 +5,17 @@ import parsers
 
 def vizdata(anglesonlyfname):
     names,times,angarray = parsers.parseangles(anglesonlyfname)
-    drop = range(9,12)+range(21,24)+range(28,30)+range(37,39)+range(42,45)+range(45,48)+range(54,57)+range(57,60)
-    inds = [ i for i in range(0,angarray.shape[1],3) if i not in drop ]
-    lags = SSR.chooseLags(angarray[:,inds],[angarray.shape[0]])
-    ns = [names[int(np.floor(j/3))] for j in inds]
-    print(ns)
-    return None
-    inds = [i for i in range(0,angarray.shape[1],3) if i not in [3*3,3*7,3*14,3*15,3*18,3*19]]
+    laginds = range(angarray.shape[1])
+    lags = SSR.chooseLags(angarray[:,laginds],[angarray.shape[0]])
+    modlags = [l if l <= 30 else 30 for l in lags[0]]
+    print(modlags)
+    drop = [9,21,42,45,54,57]
+    inds = [i for i in laginds if not i%3 and i not in drop]
     for i in inds:
-        SSRPlots.plotManifold(angarray[:,i:i+3],show=0,hold=0,style='b-',titlestr=names[int(i/3)],scatter=False,color=None)
-        SSRPlots.plotShadowManifold(angarray[:,i], 3, 10, show=1,hold=0,style='r-',titlestr=names[int(i/3)]+' reconstruction',scatter=False, color=None,smooth=1)
+        SSRPlots.plotManifold(angarray[:,i:i+3],show=0,hold=0,style='b-',titlestr=names[i/3],scatter=False,color=None)
+        SSRPlots.plotShadowManifold(angarray[:,i], 3, modlags[i], show=0,hold=0,style='r-',titlestr=names[i/3]+', M_x',scatter=False, color=None,smooth=1)
+        SSRPlots.plotShadowManifold(angarray[:,i+1], 3, modlags[i+1], show=0,hold=0,style='g-',titlestr=names[i/3]+', M_y',scatter=False, color=None,smooth=1)
+        SSRPlots.plotShadowManifold(angarray[:,i+2], 3, modlags[i+2], show=1,hold=0,style='k-',titlestr=names[i/3]+', M_z',scatter=False, color=None,smooth=1)
 
 def ssr():
     pass
