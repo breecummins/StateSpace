251af180db7460b3a9437924b693e8d734ece18e
Bree Cummins <breecummins@gmail.com>
2014-03-04 09:30:37 -0700

    cut out failed experiments

--- StateSpace/predprey.py
+++ StateSpace/predprey.py
@@ -4,45 +4,6 @@ import numpy as np
 import rk4
 import matplotlib.pyplot as plt
 
-# def predprey(t,x,A=1,B=1,C=1,D=1):
-#     dx = np.zeros(x.shape)
-#     dx[0] = x[0]*(A-x[0])-B*x[1]
-#     dx[1] = x[1]*(C-x[1])+D*x[0]
-#     return dx
-
-# def lotkavolterra(t,x,mu=1):
-#     dx = np.zeros(x.shape)
-#     dx[0] = x[0]*(1-x[1])
-#     dx[1] = mu*x[1]*(x[0]-1)
-#     return dx
-
-# def logisticlotkavolterra(t,x,A=1,B=1,C=1,D=1):
-#     dx = np.zeros(x.shape)
-#     dx[0] = x[0]*(A-A*x[0]-B*x[1])
-#     dx[1] = x[1]*(C-C*x[1]+D*x[0])
-#     return dx
-
-# def sugiharamap(t,x,A=3.8,B=0.02,C=3.5,D=0.1):
-#     dx = np.zeros(x.shape)
-#     dx[0] = x[0]*(A-A*x[0]-B*x[1])
-#     dx[1] = x[1]*(C-C*x[1]-D*x[0])
-#     return dx
-
-# def twopredoneprey(t,x,A=1,B=1,C=1,D=1,E=1,F=1,G=1,H=1,I=1):
-#     dx = np.zeros(x.shape)
-#     dx[0] = x[0]*(A-A*x[0]-B*x[1]-E*x[2])
-#     dx[1] = x[1]*(C-C*x[1]+D*x[0]-F*x[2])
-#     dx[2] = x[2]*(G-G*x[1]+H*x[0]-I*x[1])
-#     return dx
-
-# def vanocompetitivelk(t,x,rates=np.array([1,0.72,1.53,1.27]),interactionmatrix=np.array([[1,1.09,1.52,0],[0,1,0.44,1.36],[2.33,0,1,0.47],[1.21,0.51,0.35,1]])):
-#     #Vano et al (2006), Chaos in low-dimensional Lotka-Volterra models of competition
-#     return rates*x*(np.ones(x.shape) - np.sum(interactionmatrix*x,1))
-
-# def complotkavolterra3D(t,x,rates=np.array([1,0.5,1.5]),interactionmatrix=np.array([[0,1.,-1.],[-1.,0,1],[1.,-1,0]])):
-#     #Baigent notes
-#     return x*(rates + np.sum(interactionmatrix*x,1))
-
 def complotkavolterra4D(t,x,a=0,b=0,r=1,p=1,q=1):
     #Baigent notes
     rates=np.array([-1,1,-1,1])
@@ -79,26 +40,4 @@ ts = solvePredPrey([0.2,0.25,0.15,0.18],400,dt=0.025,odes=complotkavolterra4D,kw
 figstogether(ts[:,0],ts[:,1])
 figstogether(ts[:,2],ts[:,3])
 figsapart(ts)
-
-# ts = solvePredPrey([0.6,0.2],1000,dt=0.1,odes=predprey,kwargs={'A':1,'B':0.1,'C':1,'D':0.1})
-# figstogether(ts[:,0],ts[:,1])
-
-# ts = solvePredPrey([0.6,0.2],1000,dt=0.1,odes=logisticlotkavolterra,kwargs={'A':1,'B':0.5,'C':1,'D':0.1})
-# figstogether(ts[:,0],ts[:,1])
-
-# ts = solvePredPrey([1,0.105,0.099],200,dt=0.025,odes=twopredoneprey,kwargs={'A':1,'B':0.5,'C':1,'D':0.1,'E':0.5,'F':0.105,'G':1,'H':0.1,'I':0.1})
-# figsapart(ts)
-
-# ts = solvePredPrey([1,0.105,0.099],200,dt=0.025,odes=twopredoneprey,kwargs={'A':2,'B':0.495,'C':1,'D':0.1,'E':0.5,'F':0.105,'G':1,'H':0.1,'I':0.1})
-# figsapart(ts)
-
-# ts = solvePredPrey([0.6,0.2],1000,dt=0.1,odes=lotkavolterra,kwargs={'mu':0.1})
-# figstogether(ts[:,0],ts[:,1])
-
-# ts = solvePredPrey([0.55,0.5],300,dt=1,odes=sugiharamap,kwargs={})
-# figstogether(ts[:,0],ts[:,1])
-# figsapart(ts)
-
-# ts = solvePredPrey([1,0.9,1.1],100,dt=0.025,odes=complotkavolterra3D,kwargs={})
-# figstogether(ts[:,0],ts[:,1])
 
